0ba384d1b613b6db9303ece79acf4c38590f544f
WongInitiative <kw491@kill.cs.rutgers.edu>
2019-10-13 18:36:04 -0400

    trying to fix shit

--- mymalloc.c
+++ mymalloc.c
@@ -1,27 +1,25 @@
 #include "mymalloc.h"
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-
-typedef struct meta{
-	unsigned int size : 14;
-	struct* next;
-} meta;
-
-char mem[4096];
-
+//typedef struct meta{
+//	unsigned int size : 14;
+//	struct meta* next;
+//}meta;
+//
+//char mem[4096];
 
 void* mymalloc(int requested, int __LINE__, char* __FILE__){
 
 	printf("Working myMalloc");
 
+	return NULL;
 }
 
 
 void myfree(void* usrptr, int __LINE__, char* __FILE__){
 
 	printf("Working myFree");	
+
+	return NULL;
 }
 
 
--- mymalloc.h
+++ mymalloc.h
@@ -1,9 +1,9 @@
 #ifndef _MYMALLOC_H
 #define _MYMALLOC_H
-#define malloc(x) mymalloc((x), __LINE__, __FILE__)
-#define free(x) myfree((x), __LINE__, __FILE__)
+	#define malloc(x) mymalloc((x), __LINE__, __FILE__)
+	#define free(x) myfree((x), __LINE__, __FILE__)
 
-void* mymalloc(int, int, char*);
-void myfree(void*, int, char*);
+	void* mymalloc(int, int, char*);
+	void myfree(void*, int, char*);
 
 #endif
