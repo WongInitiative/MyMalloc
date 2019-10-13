4a62a6c33fbb6730e7cfede510622705f2977a57
Sameet R. Hegde <srh155@butter.cs.rutgers.edu>
2019-10-13 14:30:03 -0400

    Modified spacing

--- mymalloc.c
+++ mymalloc.c
@@ -4,17 +4,17 @@
 
 int main(int argc, char** argv){
 
-	int n = 0;
-	if (n < 10){
-	  printf("working!!");
-	  n++;
+        int n = 0;
+        if (n < 10){
+	        printf("working!!");
+                n++;
 
-        int j = 0;
-        if (j < 10){
-            printf("Working2!");
-            j++;
+                int j = 0;
+                if (j < 10){
+                        printf("Working2!");
+                        j++;
+                }
         }
-	}
 
 	return 0;
 }
