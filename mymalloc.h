153131117bfa449027bd28846f983a452099650d
WongInitiative <kw491@kill.cs.rutgers.edu>
2019-10-13 13:20:09 -0400

    Added mymalloc.h and started mymalloc.c

---
+++ mymalloc.h
@@ -0,0 +1,7 @@
+#ifndef _MYMALLOC_H
+#define _MYMALLOC_H
+	#define malloc(x) mymalloc((x), __LINE__, __FILE__)
+	#define free(x) myfree((x), __LINE__, __FILE__)
+	void* mymalloc(int, int, char*);
+	void myfree(int, int, char*);
+#endif
