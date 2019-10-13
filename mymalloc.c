e5a3a042ce7bb2c3d9fe7ad361f9df5a035e6e53
Sameet R. Hegde <srh155@butter.cs.rutgers.edu>
2019-10-13 14:16:46 -0400

    Added new myMalloc files

--- mymalloc.c
+++ mymalloc.c
@@ -3,7 +3,19 @@
 #include <unistd.h>
 
 int main(int argc, char** argv){
-	
+
+	int n = 0;
+	if (n < 10){
+        printf("working!!");
+        n++;
+
+        int j = 0;
+        if (j < 10){
+            printf("Working2!");
+            j++;
+        }
+	}
+
 	return 0;
 }
 
