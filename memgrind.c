2a64c714bc9f318ae95f12f7dfe52ec355e4d9a6
Sameet R. Hegde <srh155@butter.cs.rutgers.edu>
2019-10-13 17:57:01 -0400

    Test fxn

--- memgrind.c
+++ memgrind.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdin.h>
 
+int test();
+
+
 int main(int argc, char* argv[]){
 
+test();
 		
+return 0;
+
+}
 
 
+
+
+int test(){
+
+	printf("our test works!");
+	return 0;
 }
