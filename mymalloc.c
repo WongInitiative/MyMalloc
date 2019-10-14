a117029244121a33c5e69a0a4abc97ee9577d92e
WongInitiative <kw491@kill.cs.rutgers.edu>
2019-10-13 21:27:11 -0400

    added makefile

--- mymalloc.c
+++ mymalloc.c
@@ -1,8 +1,10 @@
 #include "mymalloc.h"
 
 void* mymalloc(int requested, int line, char* file){
-	
-	
+	int* ptr = (int*) mem;
+	*ptr = 25;
+	printf("%d\n", mem[0]);
+
 	printf("Working myMalloc");
 
 	return NULL;
