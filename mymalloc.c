de45be54e73f0e8b8b5eef7fb226d2da13244746
WongInitiative <kw491@kill.cs.rutgers.edu>
2019-10-14 09:06:08 -0400

    fully implemented mymalloc function

--- mymalloc.c
+++ mymalloc.c
@@ -1,22 +1,35 @@
 #include "mymalloc.h"
 
 void* mymalloc(int requested, int line, char* file){
-	if (mem[0] = '0')
-		printf("initizlized");
-	meta* ptr1 = (void*) mem;
-	ptr1 -> isfreeNsize = 4096 - sizeof(meta); //set 13 bits to size of free space
-	(ptr1 -> isfreeNsize) |= (1 << 13); //set 14th bit to true
-	ptr1 -> next = NULL; //set next pointer equal to null
-	if (mem[0] != '0')
-		printf("FUCK COCK DICK");
-
-
 	meta* ptr = (void*) mem;
-	int isfree = (((ptr -> isfreeNsize) >> 13) & 1);
-	int size = (ptr -> isfreeNsize) &= ~(1 << 13);
-	printf("is free is %d, size is %d, size is also%d\n", isfree, mem[0], 4096 - sizeof(*ptr));
-	printf("Working myMalloc");
-
+	if (mem[0] == '\0'){ //if no meta struct exists yet in mem, create the first one.
+		(ptr -> isfreeNsize) = 4096 - sizeof(meta); //set 13 bits to size of available space
+		(ptr -> isfreeNsize) |= (1 << 13); //set 14th bit to true (aka to 1)
+		(ptr -> next) = NULL; //set next pointer equal to null
+	}
+	if(requested > 4086)
+		printf("Error: Saturation of dynamic memory in line %d of file %s\n", line, file);	
+	while (ptr != NULL){
+		int isfree = (((ptr -> isfreeNsize) >> 13) & 1);//check 14th bit value
+		int block_size = (ptr -> isfreeNsize) &= ~(1 << 13);//set 14th bit value to 0 and get size of available space
+		if ((isfree == 1) && (block_size >= requested)){ //if the block is free AND the size of the available space is >= size of requested data
+			void* usrptr = (void*) (ptr+1);
+			int leftover_space = block_size - requested;
+			if (leftover_space > sizeof(meta)){
+				//create split off free block with unused leftover space
+				(ptr -> isfreeNsize) = requested;
+				meta* newblock = ptr + sizeof(meta) + requested;
+				(newblock -> isfreeNsize) = leftover_space - sizeof(meta);
+				(newblock -> isfreeNsize) |= (1 << 13); 
+				(newblock -> next) = NULL; 
+				(ptr -> next) = newblock;
+			}
+			(ptr -> isfreeNsize) &= ~(1 << 13); //set our isfree bit to 0 since the block is now not free
+			return usrptr; //return void pointer which points to memory space that user requested	
+		}
+		ptr = ptr -> next;
+	}	
+	printf("Error: Saturation of dynamic memory in line %d of file %s\n", line, file);	
 	return NULL;
 }
 
--- mymalloc.h
+++ mymalloc.h
@@ -9,12 +9,12 @@
 	#define malloc(x) mymalloc((x), __LINE__, __FILE__)
 	#define free(x) myfree((x), __LINE__, __FILE__)
 
-        typedef struct meta{
-                unsigned int isfreeNsize : 14;
-                struct meta* next;
-        }__attribute__((packed, aligned(1))) meta;
+        typedef struct meta{ //took a decent BIT of effort figuring out how to make this smaller (can I have more points for the pun please I'm desperate)
+                unsigned int isfreeNsize : 14; //creates bitfield of exactly 14 bits. 13 bits for the size of the usable space(which must be less than 4096), 1 bit leftover to represent if block is free or not
+                struct meta* next; //pointer to next block (aka next metadata struct)
+        }__attribute__((packed, aligned(1))) meta; //get rid of byte padding to save space :^)
 
-        char mem[4096];
+	static char mem[4096];
 	
 	void* mymalloc(int, int, char*);
 	void myfree(void*, int, char*);
