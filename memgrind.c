cc0c3d136cfd6f0704c29114fd518c42a4033e67
WongInitiative <kw491@kill.cs.rutgers.edu>
2019-10-20 12:30:15 -0400

    some workloads done

--- memgrind.c
+++ memgrind.c
@@ -13,28 +13,47 @@ void workLoad4();
 void workLoadUnique1();
 void workLoadUnique2();
 
-
+struct timeval start, end;
 
 int main(int argc, char* argv[]){
-	
+	workLoad1();
+	workLoad2();
+	workLoad3();
+	workLoad4();
+	/*int a = 0;
+	while (a <150){
+		char * ptr = (char*) malloc(sizeof(char));
+		printf("%d\n", ((meta*)mem) -> isfree);
+		free (ptr);
+		printf("%d\n", ((meta*)mem) -> isfree);
+		a++;
+	}
+	*/
+	return 0;	
 }
 /// malloc 1 byte and free immediately after
 void workLoad1(){
 
-	clock_t start, end;
+	double total_time = 0;
+	
+	int a;
+	for (a = 0; a < 100; a++){
+		gettimeofday(&start,NULL);
+		
+		int b;
+		for (b = 0; b < 150; b++){
+			char* ptr = (char*) malloc(sizeof(char));
+			free(ptr);
+			a++;
+		}
+		gettimeofday(&end, NULL);
 
-	int a = 0;
-	start = clock(); //start clock
-	while (a <150){
-		char * ptr = (char*) malloc(sizeof(char));
-		free (ptr);
-		a++;
-	}
+		total_time += (double) (end.tv_usec - start.tv_usec); 
+		
+	}	
 
-	end = clock(); //end clock
-	double total_time = (double) (end - start)/CLOCKS_PER_SEC;  //Total time
 	double avg_time = total_time/150;
-	printf("time it takes to malloc then free 1 byte is: %lf", avg_time);
+	printf("average time it takes to malloc 150 times is %lf\n", avg_time);
 
 	return;
 }
@@ -44,31 +63,129 @@ void workLoad1(){
 ///malloc 150 bytes; after 50 bytes free all of them
 void workLoad2(){
 
-	clock_t start, end;
-
+	double total_time = 0;
 	char* ptr[50];
 
-	int a, b = 0;
+	int a, b;
 
-	start = clock ();
-	while (a <150) {
-		while (b < 50){
-			a++;
-			ptr[b] = (char*) malloc(1);
+	for (a = 0; a < 100; a++) {
+		gettimeofday(&start, NULL);
+		
+		int count = 0;
+		while (count < 3){
+			for (b = 0; b < 50; b++){
+				ptr[b] = (char*) malloc(1);
+			}
+	
+			for (b = 0; b <50; b++){
+				free(ptr[b]);
+			}
+		count++;
 		}
-		b = 0;
-		while (b < 50){
-			free(ptr[b]);
-			b++;
-		}
+
+		gettimeofday(&end, NULL);
+		
+		total_time += (double)(end.tv_usec - start.tv_usec);
 	}
 
-
-	end = clock();
-	double total_time = (double) (end - start) / CLOCKS_PER_SEC;
-	double avg_time = total_time / 3; //Time it takes to malloc and free an array of 50
-	printf("Time it took to malloc and free an array of 50 ptrs is: %lf", avg_time);
+	double avg_time = total_time / 100; //Time it takes to malloc and free an array of 50
+	printf("average Time it took to malloc 50 ptrs then free all ptrs 3 times is: %lf\n", avg_time);
 }
+
+
+///Randomy choose between 1 byte malloc or freeing 1 byte free
+void workLoad3(){
+	double total_time = 0;
+	char* ptr[50];
+
+	int count = -1;
+
+	int a;
+
+	for(a = 0; a < 100; a++){
+		gettimeofday(&start, NULL);
+
+		while (count < 50){
+			int number = rand();
+			if (number % 2 == 0 && count == -1){ ///If we are trying to free from an array w/o any ptrs
+				continue;
+			} 
+			else if (number % 2 == 0 && count >= 0){
+				free(ptr[count]);
+				count--;
+			}
+			else if (number % 2 == 1) {
+				ptr[count] = (char *) malloc(1);
+				count++;
+			} 
+		}
+
+		int i = 0;
+		while (i < 50){
+			free(ptr[i]);
+			i++;
+		}
+
+		gettimeofday(&end, NULL);
+		total_time += (double) (end.tv_usec - start.tv_usec);
+		
+	}
+
+	double avg_time = total_time / 100; 
+	printf("average time it took to randomly choose between free and malloc, only freeing everything after 50 ptrs are malloced: %f\n", avg_time); 
+	
+}
+
+///Randomly mallor or free byte arrays of any size within 1 ~ 64; once 50 of these r malloced free all and end
+void workLoad4(){
+
+	double total_time = 0;
+
+	void * ptr[50];
+	int count = -1;
+
+	int a;
+	for (a = 0; a < 100; a++){
+		gettimeofday(&start, NULL);
+
+		while (count < 50){
+		
+			int classifier = rand();
+			if (classifier % 2 == 0 && count == -1){
+				continue;
+			}
+			else if (classifier % 2 == 0 && count >= 0){
+				free(ptr[count]);
+				count--;
+			}
+			else if (classifier % 2 == 1){
+				int byteSize = rand();
+				if (byteSize >= 1 && byteSize <= 64){
+					ptr[count] = (void *) malloc(byteSize);
+					count++;
+				}
+			}
+
+		}
+
+		gettimeofday(&end, NULL);
+		total_time += (double)(end.tv_usec - start.tv_usec);	
+
+	}
+
+	double average_time = total_time / 100;
+	printf("average time to randomly malloc or free byte arrays of any size within 1 to 64 bytes is: %lf", average_time);
+	
+
+}
+
+
+
+
+
+
+
+
 
 void testfunc(){
 ///General Testing:
